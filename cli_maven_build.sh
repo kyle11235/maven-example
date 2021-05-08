6b8d518786bf6c2613ddb8707fa79a71719116c5
someone <someone@example.com>
2021-05-08 15:18:33 +0800

    clear

--- cli_maven_build.sh
+++ cli_maven_build.sh
@@ -1,7 +1,10 @@
 
 BUILD_NAME=cli-maven-build
-BUILD_NUMBER=6
+BUILD_NUMBER=12
 
 jfrog rt mvn clean install --build-name=$BUILD_NAME --build-number=$BUILD_NUMBER
 jfrog rt bce $BUILD_NAME $BUILD_NUMBER
 jfrog rt bp $BUILD_NAME $BUILD_NUMBER
+
+# build scan
+jfrog rt bs $BUILD_NAME $BUILD_NUMBER --fail=false
