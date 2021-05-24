bab43b3d8906e33c53234346a866f673aadd375a
someone <someone@example.com>
2021-05-24 22:29:47 +0800

    clear

---
+++ 01_clear_whitelist.sh
@@ -0,0 +1,6 @@
+
+
+FROM=xray-project-maven-whitelist-local/com/alibaba/fastjson
+
+curl -H 'X-JFrog-Art-Api: '"$ART_API_KEY"'' \
+-X DELETE "http://182.92.214.141:8082/artifactory/$FROM"
---
+++ 02_install_by_xray_settings.sh
@@ -0,0 +1,9 @@
+
+# delete local cache
+rm -rf /Users/kyle/.m2/repository/com/alibaba/fastjson
+
+# insntall
+mvn clean install --settings /opt/apache-maven-3.6.0/conf/xray_settings.xml
+
+
+
